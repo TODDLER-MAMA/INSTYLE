30cca4ee6672cd68240b6e8cefdc0df040b3c7eb
NusayerTawhid <92975380+NusayerTawhid@users.noreply.github.com>
2025-06-25 21:29:40 +0600

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import Products from './pages/Products'
+import ProductDetail from './pages/ProductDetail'
 import Cart from './pages/Cart'
 import Checkout from './pages/Checkout'
 import Admin from './pages/Admin'
@@ -23,6 +24,7 @@ function App() {
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/products" element={<Products />} />
+                <Route path="/product/:id" element={<ProductDetail />} />
                 <Route path="/cart" element={<Cart />} />
                 <Route path="/checkout" element={<Checkout />} />
                 <Route path="/admin/login" element={<AdminLogin />} />
